1b4e960cf35ab67dac0f8c27281628066518196e
Victor <victorclovisb@hotmail.com>
2025-07-08 14:26:19 -0300

    feat: Create room component with data fetching and loading state

---
+++ src/pages/create-room.tsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+
+type GetRoomsApiResponse = Array<{
+  id: string
+  name: string
+}>
+
+export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+
+      const result: GetRoomsApiResponse = await response.json()
+
+      return result
+    }
+  })
+
+  return (
+    <div>
+      {
+        isLoading && <p>carregando</p>
+      }
+      <div className="flex flex-col gap-1">
+        {
+          data?.map(room => {
+            return (
+              <Link key={room.id} to={`/room/${room.id}`}>{room.name}</Link>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
