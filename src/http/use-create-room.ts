5a515c7e9b2aafb3d7938340032c1dee49fbaf05
Victor <victorclovisb@hotmail.com>
2025-07-10 08:59:17 -0300

    feat: Add query invalidation on successful room creation

--- src/http/use-create-room.ts
+++ src/http/use-create-room.ts
@@ -1,8 +1,10 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import type { CreateRoomRequest } from './types/create-room-request'
 import type { CreateRoomResponse } from './types/create-room-response'
 
 export function useCreateRoom() {
+  const queryClient = useQueryClient()
+
   // utiliza 'useMutation' sempre que a requisição envolver uma criação, edição ou remoção. useQuery utiliza para listagem
   return useMutation({
     mutationFn: async (data: CreateRoomRequest) => {
@@ -17,6 +19,9 @@ export function useCreateRoom() {
       const result: CreateRoomResponse = await response.json()
 
       return result
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['get-rooms'] }) // refaz as queries referenciadas no array
     },
   })
 }
