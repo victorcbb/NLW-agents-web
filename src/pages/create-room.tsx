09ff4717a47b3ab4acb06d8a478e85615ab22f08
Victor <victorclovisb@hotmail.com>
2025-07-10 00:03:08 -0300

    style: Remove unnecessary blank lines

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -2,8 +2,6 @@ import { CreateRoomForm } from "@/components/create-room-form";
 import { RoomList } from "@/components/room-list";
 
 export function CreateRoom() {
-
-
   return (
     <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-4xl">
